609d93dbbf644b787f3fc5badaa0bf29cac47b39
ayush <ayushbite@gmail.com>
2022-09-18 02:20:28 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'counterapp';
+
+  counter: number = 0;
+
+  increasecounter = () => {
+    this.counter = this.counter + 1;
+  };
+  decreasecounter = () => {
+    if (this.counter === 0) {
+      return;
+    }
+    this.counter = this.counter - 1;
+  };
+  resetcounter = () => {
+    this.counter = 0;
+  };
 }
